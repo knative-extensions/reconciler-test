abc680ab012b27c74efa162f07c50ec27be4f068
Martin Gencur <mgencur@redhat.com>
2023-02-23 12:55:16 +0100

    Revert "Make appender thread safe"
    This reverts commit f85baf5e3535a831c38c47dba08dc599ca757f2d.

--- test/e2e/environment_test.go
+++ test/e2e/environment_test.go
@@ -22,7 +22,6 @@ package e2e
 import (
 	"context"
 	"strings"
-	"sync"
 	"sync/atomic"
 	"testing"
 
@@ -115,13 +114,8 @@ func TestContextLifecycle(t *testing.T) {
 	env.Test(ctx, t, feat)
 }
 
-var mutex sync.Mutex
-
-// appender might be executed concurrently from different go routines.
 func appender(stringBuilder *strings.Builder, val string) feature.StepFn {
 	return func(ctx context.Context, t feature.T) {
-		mutex.Lock()
-		defer mutex.Unlock()
 		stringBuilder.WriteString(val)
 	}
 }
