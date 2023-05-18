25ea559e4bac142ad0fefd59163b713b449062ba
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-05-18 09:25:36 +0200

    knative_setup -> test_setup
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -21,7 +21,7 @@ readonly root_dir
 
 source "${root_dir}/vendor/knative.dev/hack/e2e-tests.sh"
 
-function knative_setup() {
+function test_setup() {
   kubectl apply -f "${root_dir}/test/config" || return $?
 
   kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml || return $?
