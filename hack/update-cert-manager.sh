71a6b2b048613ca943e5a828136bfd74f7e00a3e
Pierangelo Di Pilato <pierdipi@redhat.com>
2024-01-11 15:43:44 +0100

    Remove kubectl dependency in hack/update-cert-manager.sh (#652)
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- hack/update-cert-manager.sh
+++ hack/update-cert-manager.sh
@@ -12,9 +12,15 @@ function update_cert_manager() {
   mkdir -p third_party/cert-manager
 
   helm repo add jetstack https://charts.jetstack.io --force-update
-  kubectl create namespace --dry-run=client cert-manager -oyaml > third_party/cert-manager/00-namespace.yaml
   helm template -n cert-manager cert-manager jetstack/cert-manager --create-namespace --version "${cert_manager_version}" --set installCRDs=true > third_party/cert-manager/01-cert-manager.yaml
   helm template -n cert-manager cert-manager jetstack/trust-manager --create-namespace --version "${trust_manager_version}" --set installCRDs=true > third_party/cert-manager/02-trust-manager.yaml
+
+  cat > third_party/cert-manager/00-namespace.yaml <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: cert-manager
+EOF
 }
 
 update_cert_manager "v1.13.3" "v0.7.1"
